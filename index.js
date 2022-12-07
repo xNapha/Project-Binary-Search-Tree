e952d2be7e4117dc6d38517a03a062bfa5c3875b
xNapha <nathancai1997@gmail.com>
2022-12-07 21:22:15 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Node = (a,b = null,c =null) =>{
+    const value = a;
+    const left = b;
+    const right = c;
+    return {value, left, right}
+}
+
+const Tree = (arr) =>{
+    const root = arr;
+
+    const buildTree = (arr) =>{
+        
+    }
+
+    return {buildTree}
+}
